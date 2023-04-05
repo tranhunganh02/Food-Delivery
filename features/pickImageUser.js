764c78288664b62584083c0f2a336e04eadd9ca0
tranhunganh02 <hunganh.dh2021@gmail.com>
2023-04-05 13:11:31 +0700

    j

--- features/pickImageUser.js
+++ features/pickImageUser.js
@@ -63,7 +63,6 @@ const ButtonPickImage = ({ idUser }) => {
           // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
           switch (snapshot.state) {
             case "paused":
               console.log("Upload is paused");
