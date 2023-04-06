62f9996199da56e8efed4fe1e071fb6c7363e550
lvnh2003 <lanhnn.21it@vku.udn.vn>
2023-04-06 13:15:17 +0700

    Hùng Anh báo

---
+++ screens/Admin/FoodCRUD/AddFood.js
@@ -0,0 +1,275 @@
+
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  SafeAreaView
+} from 'react-native';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+import React, {useRef, useState} from 'react';
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+const category = [
+                {name: 'Cafe', id: '1',},
+                {name: 'Milk tea', id: '2',},
+                {name: 'Breakfast', id: '3',},
+                {name: 'Lunch tea', id: '4',},
+                {name: 'Dinner', id: '5',},
+                {name: 'Smoothies/Juices', id: '6'},
+                {name: 'Snacks', id: '7',},
+                {name: 'Noodles', id: '8',},
+              ];
+const AddFood = ({navigation}) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [imagePicker, setImagePicker] = useState("");
+  const [search, setSearch] = useState('');
+  const [clicked, setClicked] = useState(false);
+  const [data, setData] = useState(category);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const searchRef = useRef();
+  const onSearch = search => {
+    if (search !== '') {
+      let tempData = data.filter(item => {
+        return item.name.toLowerCase().indexOf(search.toLowerCase()) > -1;
+      });
+      setData(tempData);
+    } else {
+      setData(category);
+    }
+  };
+  return (
+    <SafeAreaView style={{
+       flex: 1,
+      paddingHorizontal: 20,
+      alignItems: "center",
+      justifyContent: "center",
+      paddingVertical: 10,}}>
+        <StatusBar barStyle={"dark-content"} />
+      <View style={styles.headerContainer}>
+        <TouchableOpacity
+          style={styles.headerButton}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Ionicons name="ios-arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        <Text>Your address</Text>
+        <TouchableOpacity
+          style={[
+            styles.headerButton,
+            { justifyContent: "center", alignItems: "center" },
+          ]}
+          onPress={() => {
+            ex()
+          }}
+        >
+          <AntDesign name="plus" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={{flex:1, 
+                    justifyContent:'flex-start',
+                    alignItems:'center',
+                    height:windowHeight*0.3, 
+                    width:windowWidth,
+                    marginTop:20,
+                  }}>
+      <TextInput
+        style={styles.input}
+        placeholder="Food name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Price"
+        value={price}
+        onChangeText={(number) => (setPrice(number))}
+      />
+      <TouchableOpacity
+        style={{
+          width: '80%',
+          height: 50,
+          borderRadius: 10,
+          borderWidth: 0.5,
+          alignSelf: 'center',
+          marginTop: 0,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingLeft: 15,
+          paddingRight: 15,
+        }}
+      >
+        <Text>Choose image</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          width: '80%',
+          height: 50,
+          borderRadius: 10,
+          borderWidth: 0.5,
+          alignSelf: 'center',
+          marginTop: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingLeft: 15,
+          paddingRight: 15,
+        }}
+        onPress={() => {
+          setClicked(!clicked);
+        }}>
+        <Text style={{fontWeight:'600'}}>
+          {selectedCategory == '' ? 'Select category' : selectedCategory}
+        </Text>
+        {clicked ? (
+          <Image
+            source={require('../../../assets/icon/upload.png')}
+            style={{width: 20, height: 20}}
+          />
+        ) : (
+          <Image
+            source={require('../../../assets/icon/dropdown.png')}
+            style={{width: 20, height: 20}}
+          />
+        )}
+      </TouchableOpacity>
+      {clicked ? (
+        <View
+          style={{
+            elevation: 5,
+            marginTop: 10,
+            height: 180,
+            alignSelf: 'center',
+            width: '90%',
+            backgroundColor: '#fff',
+            borderRadius: 10,
+            position:'absolute'
+          }}>
+          <TextInput
+            placeholder="Search.."
+            value={search}
+            ref={searchRef}
+            onChangeText={txt => {
+              onSearch(txt);
+              setSearch(txt);
+            }}
+            style={{
+              width: '90%',
+              height: 50,
+              alignSelf: 'center',
+              borderWidth: 0.2,
+              borderColor: '#8e8e8e',
+              borderRadius: 7,
+              marginTop: 20,
+              paddingLeft: 20,
+            }}
+          />
+
+          <FlatList
+            data={data}
+            renderItem={({item, index}) => {
+              return (
+                <TouchableOpacity
+                  style={{
+                    width: '85%',
+                    alignSelf: 'center',
+                    height: 50,
+                    justifyContent: 'center',
+                    borderBottomWidth: 0.5,
+                    borderColor: '#8e8e8e',
+                  }}
+                  onPress={() => {
+                    setSelectedCategory(item.name);
+                    setClicked(!clicked);
+                    onSearch('');
+                    setSearch('');
+                  }}>
+                  <Text style={{fontWeight: '600'}}>{item.name}</Text>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+      ) : null}
+      <TouchableOpacity
+        style={{
+          width:'80%',
+          height:50,
+          backgroundColor:'#00CC00',
+          borderRadius:10,
+          justifyContent: 'center',
+          alignItems:'center',
+          marginTop:100
+        }}
+      >
+        <Text style={{
+          color:'#fff',
+          fontSize:20
+        }}>Insert</Text>
+      </TouchableOpacity>
+      </View>
+      <View
+        style={styles.showImage}
+      >
+        <Image
+        style={{
+          width:"100%",
+          height:'100%',
+        }}
+          source={{uri:'https://c0.wallpaperflare.com/preview/879/772/974/coca-cola-the-coca-cola-company-bottle-drink.jpg'}}
+        ></Image>
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles =  StyleSheet.create({
+  headerContainer: {
+    height: windowHeight * 0.08,
+    width: "95%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 15,
+    paddingHorizontal:25
+  },
+  headerButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    width: windowWidth * 0.127,
+    height: windowHeight * 0.06,
+    backgroundColor: "#fff",
+    shadowColor: "#000000",
+    shadowOpacity: 0.5,
+    shadowRadius: 50,
+    shadowOffset: {
+      height: 0.5,
+      width: 0.5,
+    },
+  },
+   input: {
+    width: "80%",
+    padding: 10,
+    borderColor: "gray",
+    borderRadius: 10,
+    marginBottom: 10,
+    paddingLeft:15,
+    borderWidth:1
+  },
+  showImage:{
+    width:windowWidth*0.5,
+    height:windowHeight*0.3,
+    position: "absolute",
+    bottom:0
+  }
+})
+export default AddFood;
---
+++ screens/Admin/FoodCRUD/Item.js
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, View, Button, TouchableOpacity, Dimensions, Image } from 'react-native'
+import React from 'react'
+import { ListItem, Icon } from '@rneui/themed';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
+const windowWidth = Dimensions.get('window').width
+export default function Item({navigation, id, name, price, image, }) {
+  return (
+     <ListItem.Swipeable
+     style={{
+          height:150,
+          width:windowWidth*0.87,
+          justifyContent: 'center',
+          marginBottom:20
+     }}
+     leftWidth={80}
+     rightWidth={90}
+     minSlideWidth={80}
+     leftContent={(action) => (
+       <TouchableOpacity
+         style={{
+           justifyContent: 'center',
+           backgroundColor: '#f4f4f4',
+           alignItems: 'center',
+           height:'100%'
+         }}
+         onPress={()=>{
+          
+          navigation.navigate('UpdateFood')
+         }}
+       >
+         <MaterialIcons name="system-update-alt" size={24} color="black" />
+       </TouchableOpacity>
+     )}
+     rightContent={(action) => (
+          <TouchableOpacity
+         style={{
+          justifyContent: 'center',
+          backgroundColor: '#f4f4f4',
+          alignItems: 'center',
+          height:"100%"
+         }}
+          onPress={action}
+        >
+           <Feather name="trash-2" size={35} color="red" />
+        </TouchableOpacity>
+     )}
+   >
+     <Image
+      style={{
+        height: 130,
+        width: windowWidth*0.355
+      }}
+      source={{
+        uri: image
+      }}
+     >
+
+     </Image>
+     <ListItem.Content>
+       <ListItem.Title>{name}</ListItem.Title>
+       <ListItem.Subtitle>{new Intl.NumberFormat("de-DE").format(price)} VND</ListItem.Subtitle>
+     </ListItem.Content>
+     <ListItem.Chevron />
+   </ListItem.Swipeable>
+  )
+}
+
+const styles = StyleSheet.create({})
